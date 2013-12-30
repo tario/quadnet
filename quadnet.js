07358f05d681a702b1fb799ed83237ed7a1e3905
Dario Seminara <darios3@gmail.com>
2013-12-30 19:10:16 -0300

    separated js to quadnet.js, keeping js on index to minimum

---
+++ quadnet.js
@@ -0,0 +1,111 @@
+var quadnet = function(document, canvas_container, width, height) {
+  var newRenderer = function() {
+    try {
+      return new THREE.WebGLRenderer();
+    } catch(err) {
+      return new THREE.CanvasRenderer();
+    }
+  }
+  var VIEW_ANGLE = 45,
+    ASPECT = width / height,
+    NEAR = 0.1,
+    FAR = 10000;
+  var camera =
+    new THREE.PerspectiveCamera(
+      VIEW_ANGLE,
+      ASPECT,
+      NEAR,
+      FAR);
+  var scene = new THREE.Scene();
+  var material =
+    new THREE.MeshPhongMaterial(
+      {
+        color: 0xCC0000,
+        specular: 0x808080,
+        ambient: 0xffffff,
+        emissive: 0x400000,
+        shininess: 10
+      });
+
+
+  var cube = new THREE.Mesh(
+    new THREE.CubeGeometry(100,100,100),
+    material);
+
+  scene.add(cube);
+  scene.add(camera);
+
+  var light =
+    new THREE.DirectionalLight(0xFFFFFF, 1.0);
+  light.position.set(2,1,0)
+
+  scene.add(light);
+
+  camera.position.z = 500;
+
+  var origin = new THREE.Vector3(0,0,0);
+  camera.lookAt(origin);
+
+  var renderer = newRenderer();
+  renderer.setSize(width, height);
+  canvas_container.append(renderer.domElement);
+
+  var implementCameraControls = function(document) {
+
+    var mouseDown = false;
+    $(document).mousedown(function(event) {
+      mouseDown = true;
+    });
+    $(document).mouseup(function(event) {
+      mouseDown = false;
+    });
+
+    var anglex = 0;
+    var angley = 0;
+
+    var last_pagex;
+    var last_pagey;
+    var last_delta = false;
+
+    $(document).mousemove(function(event) {
+      var deltax = event.pageX - last_pagex;
+      var deltay = event.pageY - last_pagey;
+
+      if (last_delta && mouseDown) {
+        var rotationMatrix = new THREE.Matrix4();
+        var aux = new THREE.Matrix4();
+
+        anglex = anglex - deltax * 0.01;
+        angley = angley - deltay * 0.01;
+
+        if (anglex > Math.PI) anglex = anglex - Math.PI*2; 
+        if (anglex < -Math.PI) anglex = anglex + Math.PI*2; 
+        if (angley > Math.PI/2) angley = Math.PI/2;
+        if (angley < -Math.PI/2) angley = -Math.PI/2;
+
+        aux.makeRotationX(angley);
+        rotationMatrix = aux.multiply(rotationMatrix);
+
+        aux = new THREE.Matrix4();
+        aux.makeRotationY(anglex);
+        rotationMatrix = aux.multiply(rotationMatrix);
+
+        camera.position.set(0,0,500);
+        camera.position.applyMatrix4(rotationMatrix);
+        camera.lookAt(origin);
+      }
+
+      last_pagex = event.pageX;
+      last_pagey = event.pageY;
+      last_delta = true;
+    });
+  };
+
+  var anim = function() { 
+    renderer.render(scene,camera);
+    requestAnimationFrame(anim);
+  };
+
+  implementCameraControls(document);
+  requestAnimationFrame(anim);
+};
