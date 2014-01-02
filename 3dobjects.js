90ded3918caf47c72c2a10b811e20c6ce5c09ef2
Dario Seminara <darios3@gmail.com>
2014-01-02 13:14:00 -0300

    completed model of grid

--- 3dobjects.js
+++ 3dobjects.js
@@ -2,27 +2,41 @@ var Quadnet = Quadnet || {};
 Quadnet.objects = Quadnet.objects || {};
 
 Quadnet.objects.createGrid = function(square) {
+
+    var createSquare = function(x0, y0, x1, y1) {
+      var geo = new THREE.Geometry();
+      geo.vertices[0] = new THREE.Vector3(x1, y1, 0);
+      geo.vertices[1] = new THREE.Vector3(x1, y0, 0); 
+      geo.vertices[2] = new THREE.Vector3(x0, y0, 0);
+      geo.vertices[3] = new THREE.Vector3(x0, y1, 0);
+
+      geo.faces.push(new THREE.Face3(2, 1, 0));
+      geo.faces.push(new THREE.Face3(3, 2, 0));
+      geo.computeFaceNormals();
+
+      return new THREE.Mesh(geo, material);
+    };
+
     var geo =  new THREE.Geometry();
     var material =
       new THREE.MeshPhongMaterial(
         {
-          color: 0xff0000,
+          color: 0x733108,
           specular: 0x808080,
           ambient: 0xffffff,
-          emissive: 0x800000,
+          emissive: 0x733108,
           shininess: 10
         });
 
-    geo.vertices[0] = new THREE.Vector3(square.right, square.top, 0);
-    geo.vertices[1] = new THREE.Vector3(square.right, square.bottom, 0); 
-    geo.vertices[2] = new THREE.Vector3(square.left, square.bottom, 0);
-    geo.vertices[3] = new THREE.Vector3(square.left, square.top, 0);
+    var deltax = square.right - square.left;
+    var deltay = square.top - square.bottom;
+    var parentObject = new THREE.Object3D();
+    for (var i = 0; i<10; i++) {
+      parentObject.add(createSquare(square.left + deltax * i / 9, square.bottom, square.left + deltax * i / 9 + 2, square.top));
+      parentObject.add(createSquare(square.left, square.bottom + deltay * i / 9, square.right, square.bottom + deltay * i / 9 + 2));
+    };
 
-    geo.faces.push(new THREE.Face3(2, 1, 0));
-    geo.faces.push(new THREE.Face3(3, 2, 0));
-    geo.computeFaceNormals();
-
-    return new THREE.Mesh(geo, material);
+    return parentObject;
 }
 
 
