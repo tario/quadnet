eeada51e0040a43f76f048a503a2b6947f8b6d4d
Dario Seminara <darios3@gmail.com>
2014-01-25 23:28:21 -0300

    optimized generation of explosion visual effect

--- quadnet.js
+++ quadnet.js
@@ -503,10 +503,16 @@ var quadnet = function(document, canvas_container) {
             for (var i=0; i<12; i++)
               spawnParticle(createWhiteParticle, obj.x, obj.y, (Math.random()-0.5)*0.4 ,(Math.random()-0.5)*0.4);
 
+            var cos_t = Math.cos(Math.PI/16);
+            var sin_t = Math.sin(Math.PI/16);
+            var dx = 1;
+            var dy = 0;
             for (var i=0; i<32; i++) {
               (function() {
-                var dx = Math.cos(i*Math.PI/16);
-                var dy = Math.sin(i*Math.PI/16);
+                var dx_ = dx;
+                var dy_ = dy;
+                dx = dx_ * cos_t - dy_ * sin_t;
+                dy = dx_ * sin_t + dy_ * cos_t;
                 spawnParticle(createRedParticle, obj.x, obj.y, dx*0.07 ,dy*0.07);
                 spawnParticle(createRedParticle, obj.x, obj.y, dx*0.085 ,dy*0.085);
                 spawnParticle(createRedParticle, obj.x, obj.y, dx*0.1 ,dy*0.1);
