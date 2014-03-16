a90c80566d940d606da7fc7703e346e7171206d8
Dario Seminara <darios3@gmail.com>
2014-03-16 19:27:38 -0300

    optimized usage of firebase for highscores

--- js/highscore.js
+++ js/highscore.js
@@ -1,22 +1,6 @@
 var Quadnet = Quadnet||{};
 Quadnet.highscore=Quadnet.highscore||(function(){
-
-  var createChangeCallback = function(cb, newscore) {
-    return function(snapshot) {
-      var array = [];
-      var value = snapshot.val();
-      if (newscore) {
-        array.push({score: newscore, shouldInputName: true});
-      }
-
-      for (key in value) {
-        var obj = value[key];
-        array.push({name: obj.name, score: obj.score});
-      } 
-
-      cb(array.sort(function(x,y){ return y.score - x.score; }));
-    }
-  };
+  var scoresArray = [];
 
   var highscoreFirebase = function() {
     return new Firebase("https://scorching-fire-8890.firebaseio.com/highscores");
@@ -24,33 +8,37 @@ Quadnet.highscore=Quadnet.highscore||(function(){
 
   return {
     oneChangedAll: function(cb, newscore) {
-      var firebase = highscoreFirebase();
-      firebase.once("value", createChangeCallback(cb, newscore));
+      var newArray = scoresArray;
+      if (newscore) {
+        newArray = scoresArray.slice(0);
+        newArray.push({score: newscore, shouldInputName: true});
+        newArray = newArray.sort(function(x,y){ return y.score - x.score; });
+      }
+
+      cb(newArray);
     },
 
-    onChangedAll: function(cb, newscore) {
+    onChangedAll: function(cb) {
       var firebase = highscoreFirebase();
-      firebase.on("value", createChangeCallback(cb, newscore));
+      firebase.on("child_added", function(newData) {
+        scoresArray.push({name: newData.val().name, score: newData.val().score});
+        scoresArray = scoresArray.sort(function(x,y){ return y.score - x.score; })
+        cb(scoresArray);
+      });
     },
 
     shouldEnterHighscore: function(newscore, yes, no) {
-      var firebase = highscoreFirebase();
-      firebase.once("value", function(snapshot) {
-        var array = [];
-        var entries = 0;
-        var value = snapshot.val();
-        for (key in value) {
-          entries++;
-          var obj = value[key];
-          var score = obj.score;
-          if (newscore > score) {
-            yes();
-            return;
-          }
-        }
+      if (scoresArray.length < 10) {
+        yes(); return;
+      }
 
-        (entries < 10 ? yes : no)();
-      });
+      for (var i=0; i<scoresArray.length; i++) {
+        if (newscore > scoresArray[i].score) {
+          yes(); return;
+        }
+      }
+
+      no();
     },
 
     insert: function(name, score) {
